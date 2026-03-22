2c2de1b14cebe7b495ebcff9282978dedccc4bf3
Datanoise <git@datanoise.org>
2026-03-22 05:35:27 +0100

    chore: volume 50%

--- main/main.c
+++ main/main.c
@@ -285,7 +285,7 @@ static esp_err_t es8311_codec_init(void)
     }
 
     /* Set output volume (0-100) */
-    ret = es8311_voice_volume_set(codec, 35, NULL);
+    ret = es8311_voice_volume_set(codec, 50, NULL);
     if (ret != ESP_OK) {
         ESP_LOGW(TAG, "ES8311 volume set failed: %s", esp_err_to_name(ret));
     }

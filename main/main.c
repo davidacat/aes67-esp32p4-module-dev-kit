0631f906e4d90d535336c4ff3274ee6ff3b4ac01
Datanoise <git@datanoise.org>
2026-03-22 05:52:12 +0100

    test: slow TX to 10ms pacing to test if EMAC TX/RX contention causes 20% loss

--- main/main.c
+++ main/main.c
@@ -449,8 +449,8 @@ void app_main(void)
         aes67_rtp_source_write(src_info.rtp_stream,
                                tone_buf, samples_per_packet);
 
-        /* 1ms pacing. At IDLE+1 priority the scheduler will preempt
-         * us for IDLE task watchdog resets. */
-        vTaskDelay(pdMS_TO_TICKS(1));
+        /* Slow down TX to reduce EMAC contention with RX.
+         * At 10ms pacing we send 100 pkts/sec instead of 1000. */
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }

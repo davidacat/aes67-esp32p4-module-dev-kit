500b6b9636c1fe28ee35de05d2c0b6c824464853
Datanoise <git@datanoise.org>
2026-03-23 13:25:18 +0100

    fix: pin httpd to core 1, refresh gzipped HTML

--- components/aes67/src/aes67_webui.c
+++ components/aes67/src/aes67_webui.c
@@ -801,6 +801,7 @@ esp_err_t aes67_webui_start(aes67_webui_handle_t handle)
     config.stack_size = 8192;
     config.lru_purge_enable = true;
     config.max_open_sockets = 4;
+    config.core_id = 1;  /* Run HTTP server on core 1 (audio on core 0) */
 
     esp_err_t ret = httpd_start(&handle->httpd, &config);
     if (ret != ESP_OK) {

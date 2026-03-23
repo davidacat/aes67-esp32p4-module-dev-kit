9aad5a514ebc449d11d41399003e9e51766d7f03
Datanoise <git@datanoise.org>
2026-03-23 15:16:09 +0100

    fix: WebSocket browser connection, multiline SDP input
    - Disable HTTP keep-alive to free sockets for WebSocket
    - Add Connection: close on page and favicon responses
    - Increase max_open_sockets back to 7 (system has 16 now)
    - SDP subscribe input is now a multiline textarea

--- components/aes67/src/aes67_webui.c
+++ components/aes67/src/aes67_webui.c
@@ -81,6 +81,7 @@ static esp_err_t handler_index(httpd_req_t *req)
     size_t len = index_html_gz_end - index_html_gz_start;
     httpd_resp_set_type(req, "text/html");
     httpd_resp_set_hdr(req, "Content-Encoding", "gzip");
+    httpd_resp_set_hdr(req, "Connection", "close");
     return httpd_resp_send(req, (const char *)index_html_gz_start, len);
 }
 
@@ -89,6 +90,7 @@ static esp_err_t handler_index(httpd_req_t *req)
 static esp_err_t handler_favicon(httpd_req_t *req)
 {
     httpd_resp_set_status(req, "204 No Content");
+    httpd_resp_set_hdr(req, "Connection", "close");
     return httpd_resp_send(req, NULL, 0);
 }
 
@@ -806,7 +808,8 @@ esp_err_t aes67_webui_start(aes67_webui_handle_t handle)
     config.max_uri_handlers = 16;
     config.stack_size = 8192;
     config.lru_purge_enable = true;
-    config.max_open_sockets = 4;
+    config.max_open_sockets = 7;
+    config.keep_alive_enable = false;  /* Close HTTP conns immediately, free sockets for WS */
     config.core_id = 1;  /* Run HTTP server on core 1 (audio on core 0) */
 
     esp_err_t ret = httpd_start(&handle->httpd, &config);
